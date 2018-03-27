b76202c0de076a7cf82bac59f4f2919e5dfcad46
Igor Machado <igor.machado@gmail.com>
2018-03-27 14:13:42 -0300

    created Start example

---
+++ code-mappings/Start.cs
@@ -0,0 +1,10 @@
+using Neo.SmartContract.Framework.Services.Neo;
+namespace Neo.SmartContract
+{
+    public class Start : Framework.SmartContract
+    {
+        public static void Main()
+        {
+        }
+    }
+}
